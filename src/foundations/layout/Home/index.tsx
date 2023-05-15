e0ad00f47382b0f94b85f439e79ac2c4b097b0a7
DevArthurSantos <arthursantos.s@hotmail.com>
2023-05-15 05:49:17 -0300

    add: adding Container to the layout

--- src/foundations/layout/Home/index.tsx
+++ src/foundations/layout/Home/index.tsx
@@ -1,10 +1,11 @@
+import S from "./styled"
 import Header from "@foundations/components/patterns/Header";
 
 function HomePageLayout(): JSX.Element {
   return (
-    <>
+    <S.Container>
       <Header />
-    </>
+    </S.Container>
   );
 }
 
---
+++ src/foundations/layout/Home/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Styleds = {
+  Container: styled.div`
+  max-width: 1920px;
+  margin: 0 auto;
+  `,
+};
+
+export default Styleds;
